3beb86e91a0e8f1a500cce6957acfbadd70eb351
丁少华 <960423114@qq.com>
2025-10-28 18:54:55 +0800

    feat: 集成shadcn和eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'eslint/config'
-import tseslint from '@electron-toolkit/eslint-config-ts'
-import eslintConfigPrettier from '@electron-toolkit/eslint-config-prettier'
-import eslintPluginReact from 'eslint-plugin-react'
-import eslintPluginReactHooks from 'eslint-plugin-react-hooks'
-import eslintPluginReactRefresh from 'eslint-plugin-react-refresh'
+import { defineConfig } from 'eslint/config';
+import eslintPluginReact from 'eslint-plugin-react';
+import tseslint from '@electron-toolkit/eslint-config-ts';
+import eslintPluginReactHooks from 'eslint-plugin-react-hooks';
+import eslintPluginReactRefresh from 'eslint-plugin-react-refresh';
+// import eslintConfigPrettier from '@electron-toolkit/eslint-config-prettier';
 
 export default defineConfig(
   { ignores: ['**/node_modules', '**/dist', '**/out'] },
@@ -13,20 +13,21 @@ export default defineConfig(
   {
     settings: {
       react: {
-        version: 'detect'
-      }
-    }
+        version: 'detect',
+      },
+    },
   },
   {
     files: ['**/*.{ts,tsx}'],
     plugins: {
       'react-hooks': eslintPluginReactHooks,
-      'react-refresh': eslintPluginReactRefresh
+      'react-refresh': eslintPluginReactRefresh,
     },
     rules: {
       ...eslintPluginReactHooks.configs.recommended.rules,
-      ...eslintPluginReactRefresh.configs.vite.rules
-    }
+      ...eslintPluginReactRefresh.configs.vite.rules,
+      '@typescript-eslint/explicit-function-return-type': 'off',
+    },
   },
-  eslintConfigPrettier
-)
+  // eslintConfigPrettier,  // 关闭eslint中关于prettier的检查
+);
