ec0762f2a1b5ae39eaa46102cbee98b7cd4673eb
丁少华 <960423114@qq.com>
2025-11-17 01:30:15 +0800

    Update index.ts

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,7 +1,7 @@
-import { app, shell, BrowserWindow, ipcMain } from 'electron'
-import { join } from 'path'
-import { electronApp, is } from '@electron-toolkit/utils'
-import icon from '../../resources/icon.png?asset'
+import { join } from 'path';
+import icon from '../../resources/icon.png?asset';
+import { app, shell, BrowserWindow, ipcMain } from 'electron';
+import { electronApp, optimizer, is } from '@electron-toolkit/utils';
 
 function createWindow(): void {
   // Create the browser window.
@@ -13,25 +13,25 @@ function createWindow(): void {
     ...(process.platform === 'linux' ? { icon } : {}),
     webPreferences: {
       preload: join(__dirname, '../preload/index.js'),
-      sandbox: false
-    }
-  })
+      sandbox: false,
+    },
+  });
 
   mainWindow.on('ready-to-show', () => {
-    mainWindow.show()
-  })
+    mainWindow.show();
+  });
 
   mainWindow.webContents.setWindowOpenHandler((details) => {
-    shell.openExternal(details.url)
-    return { action: 'deny' }
-  })
+    shell.openExternal(details.url);
+    return { action: 'deny' };
+  });
 
   // HMR for renderer base on electron-vite cli.
   // Load the remote URL for development or the local html file for production.
   if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
-    mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
+    mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL']);
   } else {
-    mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
+    mainWindow.loadFile(join(__dirname, '../renderer/index.html'));
   }
 }
 
@@ -40,51 +40,50 @@ function createWindow(): void {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   // Set app user model id for windows
-  electronApp.setAppUserModelId('com.electron')
+  electronApp.setAppUserModelId('com.electron');
 
   // Default open or close DevTools by F12 in development
   // and ignore CommandOrControl + R in production.
   // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
-  // app.on('browser-window-created', (_, window) => {
-  //   optimizer.watchWindowShortcuts(window)
-  // })
+  app.on('browser-window-created', (_, window) => {
+    optimizer.watchWindowShortcuts(window);
+  });
 
   // 允许在生产环境下也能使用F12打开开发者工具
-  app.on('browser-window-created', (_, window) => {
-    window.webContents.on('before-input-event', (event, input) => {
-      if (input.key === 'F12') {
-        if (window.webContents.isDevToolsOpened()) {
-          window.webContents.closeDevTools()
-        } else {
-          window.webContents.openDevTools()
-        }
-        event.preventDefault()
-      }
-    })
-  })
-
+  // app.on('browser-window-created', (_, window) => {
+  //   window.webContents.on('before-input-event', (event, input) => {
+  //     if (input.key === 'F12') {
+  //       if (window.webContents.isDevToolsOpened()) {
+  //         window.webContents.closeDevTools()
+  //       } else {
+  //         window.webContents.openDevTools()
+  //       }
+  //       event.preventDefault()
+  //     }
+  //   })
+  // })
 
   // IPC test
-  ipcMain.on('ping', () => console.log('pong'))
-  import('./icp-handlers')
+  ipcMain.on('ping', () => console.log('pong'));
+  import('./icp-handlers');
 
-  createWindow()
+  createWindow();
 
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
